da73826328b0ff9488f8eccb7986ddfcb5b3d66d
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-11-11 17:17:06 +0530

    Add files via upload

---
+++ AndOrOperators/main.py
@@ -0,0 +1,23 @@
+a = 10
+b = 8
+c= 0 
+
+if a and b and c:
+    print("All numbers have boolean value as true")
+else:
+    print("All numbers have boolean value as false")
+
+
+x = 10
+y = -10
+z = 0
+
+if x > 0 or y > 0:
+    print("Either number is greater than 0 ")
+else:
+    print("No number is greater than 0")
+
+if y > 0 or z > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
